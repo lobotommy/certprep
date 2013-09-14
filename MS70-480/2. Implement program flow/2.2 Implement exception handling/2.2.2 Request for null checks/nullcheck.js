f6ba00ec5962ae8bd1f30266e46a0bcba020a51d
Tommy Wendelborg <wendelborg@gmail.com>
2013-09-14 12:04:52 +0200

    Started adding socket files

--- MS70-480/2. Implement program flow/2.2 Implement exception handling/2.2.2 Request for null checks/nullcheck.js
+++ MS70-480/2. Implement program flow/2.2 Implement exception handling/2.2.2 Request for null checks/nullcheck.js
@@ -1,4 +1,16 @@
 window.onload = init;
 function init() {
 
+    var hello;
+    if(hello === null) {
+        hello = "null?";
+    }
+    else if(!hello) {
+        hello = "hello!";
+    }
+
+    document.writeln(hello + "<br/>");
+
+    var check;
+    document.writeln((check === undefined).toString());
 }
