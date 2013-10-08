39a556ed0410eda1d5a66ca8a951e229ceb35b71
Lobotommy <wendelborg@gmail.com>
2013-10-08 10:13:19 +0200

    Chapter 5 Exercise 1

---
+++ 70-480 Training Guide/Ch5/WebCalculator/WebCalculator/Scripts/CalculatorLibrary.js
@@ -0,0 +1,55 @@
+﻿var txtInput;
+var txtResult;
+var results;
+
+function initialize() {
+    for (var i = 0; i < 10; i++) {
+        document.getElementById('btn' + i)
+         .addEventListener('click', numberClick, false);
+    }
+    txtInput = document.getElementById('txtInput');
+    txtResult = document.getElementById('txtResult');
+    results = document.getElementById('results');
+
+    document.getElementById('btnPlus')
+         .addEventListener('click', plusClick, false);
+    document.getElementById('btnMinus')
+         .addEventListener('click', minusClick, false);
+    document.getElementById('btnClearEntry')
+         .addEventListener('click', clearEntry, false);
+    document.getElementById('btnClear').addEventListener('click', clear, false);
+    clear();
+}
+
+function clear() {
+    txtInput.value = '0';
+    txtResult.value = '0';
+}
+
+function clearEntry() {
+    txtInput.value = '0';
+}
+function plusClick() {
+    var child = document.createElement('li');
+    child.innerHTML = "+" + txtInput.value;
+    results.appendChild(child);
+    
+    txtResult.value = Number(txtResult.value) + Number(txtInput.value);
+    
+    clearEntry();
+}
+
+function minusClick() {
+    var child = document.createElement('li');
+    child.innerHTML = "-" + txtInput.value;
+    results.appendChild(child);
+    
+    txtResult.value = Number(txtResult.value) - Number(txtInput.value);
+
+    clearEntry();
+}
+
+function numberClick() {
+    txtInput.value = txtInput.value == '0' ?
+       this.innerText : txtInput.value + this.innerText;
+}
