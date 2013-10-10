7250b760c6e4e7f2833649adc29e318f85d3eaba
Lobotommy <wendelborg@gmail.com>
2013-10-10 21:18:59 +0200

    Ch8 Ex2

---
+++ 70-480 Training Guide/Ch8/ConceptosWeb/WebCalculator/WebCalculator/Scripts/ContactUs.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="jquery-2.0.3.min.js" />
+
+$(document).ready(function () {
+    $('#submit').on('click', callServer);
+});
+
+function callServer() {
+    var data = $('form[name="ContactForm"]').serialize();
+    $.post('/ContactService', data, function (returnObject) {
+        $('#result').html(returnObject.result);
+    }, 'json');
+}
--- 70-480 Training Guide/Ch8/ConceptosWeb/index.js
+++ 70-480 Training Guide/Ch8/ConceptosWeb/index.js
@@ -21,7 +21,24 @@ app.post('/ContactMessage', function (request, response) {
         response.write('Thank you, ' + firstName + ' ' + lastName + "<br/>");
         response.write('We will contact you at ' + email + '</br>');
         response.end('Your message: ' + message);
-        console.log('Handled request for ' + firstName + ' ' + lastName);   
+        console.log('MSG: Handled request for ' + firstName + ' ' + lastName);   
+    });
+});
+
+app.post('/ContactService', function(request, response) {
+    var form = new formidable.IncomingForm();
+    form.parse(request, function(err, fields) {
+        var lastName = fields.lastName;
+        var firstName = fields.firstName;
+        var email = fields.email;
+        var message = fields.message;
+        var result =
+                'Thank you, ' + firstName + ' ' + lastName + "<br/>" +
+                'We will contact you at ' + email + '</br>' +
+                'Your message: ' + message;
+        response.writeHead('200', { 'Content-Type': 'application/json' });
+        response.end('{ "result" : "' + result + '"}');
+        console.log('SVC: Handled request for ' + firstName + ' ' + lastName);
     });
 });
 
