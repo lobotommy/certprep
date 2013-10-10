1ae068f9b66e4df9f305e33ad0e78ca3daff9362
Lobotommy <wendelborg@gmail.com>
2013-10-10 07:04:23 +0200

    Node up and running

---
+++ 70-480 Training Guide/Ch8/ConceptosWeb/README.md
@@ -0,0 +1,1 @@
+Simple server for the calculator
---
+++ 70-480 Training Guide/Ch8/ConceptosWeb/index.js
@@ -0,0 +1,16 @@
+﻿var express = require('express');
+var app = express();
+var formidable = require('formidable');
+
+var dir = express.static(__dirname + '/WebCalculator/WebCalculator');
+app.use(dir);
+
+
+
+app.get('/', function (request, response) {
+    response.redirect('default.html');
+});
+
+var port = 8080;
+app.listen(port);
+console.log("listening on port " + port);
